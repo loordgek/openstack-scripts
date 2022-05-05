e575afb96882884ba5901b174bd8d84ae9e361cf
loordgek <stefanhempenius@hotmail.com>
2022-05-05 20:14:03 +0200

    remove Octa

--- install.sh
+++ install.sh
@@ -43,14 +43,6 @@ function install-common-packages() {
 	echo "About to configure APT for Ocata"
 	sleep 3
 	apt-get install software-properties-common -y
-	ubuntu_version=`lsb_release -sr`
-	if [ "$ubuntu_version" == "17.04" ] || [ "$ubuntu_version" == "16.04" ]
-	then
-		add-apt-repository cloud-archive:ocata
-	else
-		echo "Ocata release supported only on Zesty (17.04) and Xenial (16.04)"
-		exit 1;
-	fi
 
 	echo "Doing full system update"
 	sleep 3
